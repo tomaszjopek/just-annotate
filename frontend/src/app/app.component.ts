034323c402730359ea33446d13b575068237362b
Tomasz Jopek <tom.jopek@gmail.com>
2024-03-04 19:22:56 +0100

    ANN-7 | basic frontend app layout setup with angular material

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatSidenav, MatSidenavContainer } from "@angular/material/sidenav";
+import { MatNavList } from "@angular/material/list";
+import { MatCard, MatCardContent } from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatSidenavContainer, MatSidenav, MatNavList, MatCard, MatCardContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  @ViewChild("sidenav")
+  sidenav: MatSidenav | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  openSidenav() {
+    this.sidenav?.open();
+  }
 }

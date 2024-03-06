eba388ad4f3734a901f0e0ad64659bf722e25655
Tomasz Jopek <tom.jopek@gmail.com>
2024-03-06 20:30:51 +0100

    ANN-8 | working SSO with frontend

--- just-annotate-backend/src/main/kotlin/pl/itj/dev/justannotatebackend/infrastructure/SecurityConfig.kt
+++ just-annotate-backend/src/main/kotlin/pl/itj/dev/justannotatebackend/infrastructure/SecurityConfig.kt
@@ -2,19 +2,13 @@ package pl.itj.dev.justannotatebackend.infrastructure
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity.http
-import org.springframework.security.oauth2.core.authorization.OAuth2ReactiveAuthorizationManagers.hasScope
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsConfigurationSource
-import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 
@@ -23,31 +17,13 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 @EnableReactiveMethodSecurity
 class SecurityConfig {
 
-//    @Bean
-//    fun corsWebFilter(): CorsWebFilter {
-//        val corsConfig = CorsConfiguration()
-//        corsConfig.allowedOrigins = mutableListOf("http://localhost:4200")
-//        corsConfig.allowCredentials = true
-//        corsConfig.maxAge = 8000L
-//        corsConfig.addAllowedMethod("GET")
-//        corsConfig.addAllowedMethod("POST")
-//        corsConfig.addAllowedMethod("PUT")
-//        corsConfig.addAllowedMethod("PATCH")
-//        corsConfig.addAllowedMethod("DELETE")
-//        corsConfig.addAllowedMethod("OPTIONS")
-//        corsConfig.addAllowedHeader("Authorization")
-//
-//        val source = UrlBasedCorsConfigurationSource()
-//        source.registerCorsConfiguration("/**", corsConfig)
-//
-//        return CorsWebFilter(source)
-//    }
-
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
         configuration.allowedOrigins = listOf("http://localhost:4200")
-//        configuration.allowedMethods = listOf("GET", "POST")
+        configuration.allowCredentials = true
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
@@ -72,11 +48,6 @@ class SecurityConfig {
         http.oauth2ResourceServer { it.jwt {  } }
 
         return http.build()
-    }
-
-    @Bean
-    fun jwtDecoder(): ReactiveJwtDecoder {
-        return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8081/auth/realms/just-annotate")
     }
 
 }
